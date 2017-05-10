e4ff122b010f9a743ce9e1c41417667aa69e3a01
Shivang Bhandari <pump382156@gmail.com>
2017-05-10 22:34:01 +0530

    Save functionality added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
     var md = document.getElementById('input');
     var output = document.getElementById('markdown');
-    md.onkeyup = function(){
-        var parsed = marked(md.value);
-        output.innerHTML="";
-    output.innerHTML = parsed;
-};
+    md.onkeyup = function() {
+      var parsed = marked(md.value);
+      output.innerHTML = "";
+      output.innerHTML = parsed;
+    };
+
+    function download(filename, type) {
+    var md = document.getElementById('input').value;
+    var file = new Blob([md], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
